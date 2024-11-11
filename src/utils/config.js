71b0c753770ef382516e9ce4ce9707fe230709eb
Nikola Ivanovic <ivanovic.nikola.ns@gmail.com>
2024-11-11 13:36:34 +0100

    Added env.eample file

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,23 +1,3 @@
-export const API_URL = "https://laterz.api.exebyte.io/api/";
-export const REFRESH_TOKEN_URL = "auth/refresh-token";
-export const LOGIN_URL = "auth/login";
-1;
-export const FACEBOOK_LOGIN_URL = "auth/facebook/login";
-export const FACEBOOK_REGISTER_URL = "auth/facebook/register";
-export const REGISTER_URL = "auth/register";
-export const GET_USER_SETTINGS_API = "user/settings/getUser";
-export const UPDATE_USER_SETTINGS_API = "user/settings";
-export const DELETE_USER_URL = "user/settings/deactivate";
-export const FORGOT_PASSWORD_REQUEST_URL = "auth/forgot-password/request";
-export const VERIFY_USER_CODE_URL = "auth/forgot-password/verify";
-export const CHANGE_PASSWORD_URL = "auth/forgot-password/change";
-export const OUTFITS_URL = "outfits";
-export const CREATE_OUTFIT_URL = "outfits/new";
-export const CHAT_URL = "chat";
-export const ADD_CHAT_URL = "chat/getChat";
-
-export const FACEBOOK_LOGIN_APP_ID = "3046302348851990";
-
 export const navLinks = [
   { path: "login", text: "login", status: "public" },
   { path: "register", text: "register", status: "public" },
