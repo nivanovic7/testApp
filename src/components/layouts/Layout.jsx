899c0f32ec8da510584249ff24facd27a5f2b08d
Nikola Ivanovic <ivanovic.nikola.ns@gmail.com>
2024-11-13 11:38:28 +0100

    Added footer and header

--- src/components/layouts/Layout.jsx
+++ src/components/layouts/Layout.jsx
@@ -9,6 +9,7 @@ import Profile from "../../pages/profile/Profile";
 import CreatePost from "../../app/pages/createPost/CreatePost";
 import Inbox from "../../pages/inbox/Inbox";
 import Header from "../headers/header/Header";
+import Footer from "../footers/footer/Footer";
 
 function Layout() {
   return (
@@ -16,24 +17,22 @@ function Layout() {
       <Header />
       <Routes>
         <Route path="/" element={<Navigate to="login" />} />
-
         <Route element={<PersistedLogin />}>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Route>
-
         <Route element={<GuardRoute />}>
           <Route path="/dashboard" element={<Dashboard />}>
             <Route index element={<Navigate to="map" />} />
             <Route path="map" element={<Map />} />
             <Route />
           </Route>
-
           <Route path="/profile" element={<Profile />} />
           <Route path="/createPost" element={<CreatePost />} />
           <Route path="/inbox" element={<Inbox />} />
         </Route>
       </Routes>
+      <Footer />
     </div>
   );
 }
