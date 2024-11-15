3027eb2b19dee1cebcc3c0eca64817af95104388
Nikola Ivanovic <ivanovic.nikola.ns@gmail.com>
2024-11-15 14:07:18 +0100

    Removed dependecies that project doesnt need

--- src/components/facebookLoginButton/FacebookLoginButton.jsx
+++ src/components/facebookLoginButton/FacebookLoginButton.jsx
@@ -1,49 +1,49 @@
-import FacebookLogin from "react-facebook-login";
+// import FacebookLogin from "react-facebook-login";
 
-import { getRegisterCredentialsFromFB } from "../../utils/helpers";
-import {
-  useFacebookLoginMutation,
-  useFacebookRegisterMutation,
-} from "../../app/api/authApislice";
-import { useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import { setCredentials } from "../../app/slices/authSlice";
+// import { getRegisterCredentialsFromFB } from "../../utils/helpers";
+// import {
+//   useFacebookLoginMutation,
+//   useFacebookRegisterMutation,
+// } from "../../app/api/authApislice";
+// import { useNavigate } from "react-router-dom";
+// import { useDispatch } from "react-redux";
+// import { setCredentials } from "../../app/slices/authSlice";
 
-function FacebookLoginButton() {
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const [facebookLogin] = useFacebookLoginMutation();
-  const [facebookRegister] = useFacebookRegisterMutation();
+// function FacebookLoginButton() {
+//   const navigate = useNavigate();
+//   const dispatch = useDispatch();
+//   const [facebookLogin] = useFacebookLoginMutation();
+//   const [facebookRegister] = useFacebookRegisterMutation();
 
-  async function responseFacebook(res) {
-    const credentials = getRegisterCredentialsFromFB(res);
+//   async function responseFacebook(res) {
+//     const credentials = getRegisterCredentialsFromFB(res);
 
-    try {
-      await facebookRegister(credentials).unwrap();
-      navigate("/profile");
-    } catch (registerError) {
-      if (registerError.status === 409) {
-        try {
-          const { data: userData } = await facebookLogin(credentials).unwrap();
-          dispatch(setCredentials(userData));
-          navigate("/profile");
-        } catch (loginError) {
-          console.log(loginError, "FB LOGIN NOT SUCCESSFULL");
-        }
-      } else {
-        console.log(registerError, "FB REGISTRATION NOT SUCCESSFULL");
-      }
-    }
-  }
+//     try {
+//       await facebookRegister(credentials).unwrap();
+//       navigate("/profile");
+//     } catch (registerError) {
+//       if (registerError.status === 409) {
+//         try {
+//           const { data: userData } = await facebookLogin(credentials).unwrap();
+//           dispatch(setCredentials(userData));
+//           navigate("/profile");
+//         } catch (loginError) {
+//           console.log(loginError, "FB LOGIN NOT SUCCESSFULL");
+//         }
+//       } else {
+//         console.log(registerError, "FB REGISTRATION NOT SUCCESSFULL");
+//       }
+//     }
+//   }
 
-  return (
-    <FacebookLogin
-      appId={import.meta.REACT_APP_FACEBOOK_LOGIN_APP_ID}
-      autoLoad={false}
-      fields="email, name, picture, gender, first_name"
-      callback={responseFacebook}
-    />
-  );
-}
+//   return (
+//     <FacebookLogin
+//       appId={import.meta.REACT_APP_FACEBOOK_LOGIN_APP_ID}
+//       autoLoad={false}
+//       fields="email, name, picture, gender, first_name"
+//       callback={responseFacebook}
+//     />
+//   );
+// }
 
-export default FacebookLoginButton;
+// export default FacebookLoginButton;
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -5,7 +5,7 @@ import { Link, useNavigate } from "react-router-dom";
 import Form from "../../components/Form/Form";
 import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
 import Input from "../../components/input/Input";
-import FacebookLoginButton from "../../components/facebookLoginButton/FacebookLoginButton";
+// import FacebookLoginButton from "../../components/facebookLoginButton/FacebookLoginButton";
 import { useLoginMutation } from "../../app/api/authApislice";
 import { setCredentials } from "../../app/slices/authSlice";
 
@@ -52,7 +52,7 @@ function Login() {
       />
 
       <button disabled={isLoading}>{isLoading ? "Loading..." : "Login"}</button>
-      <FacebookLoginButton />
+      {/* <FacebookLoginButton /> */}
       <Link to="/register">You don&apos;t have account? Reqister!</Link>
     </Form>
   );
