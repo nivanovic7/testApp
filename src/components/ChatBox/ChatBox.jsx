bba472c4d96013b4d6e539122c91bd6ae75abb72
Nikola Ivanovic <ivanovic.nikola.ns@gmail.com>
2024-11-29 12:59:34 +0100

    Refactor - split content into functions

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -12,23 +12,27 @@ function ChatBox({ chatId, setSelectedChatId, isSmallScreen }) {
     setNewMessages([]);
   }, [chatId]);
 
-  return (
-    <div className={styles.container}>
-      {isSmallScreen && (
-        <button onClick={() => setSelectedChatId(null)}>Back</button>
-      )}
-      <div className={styles.chat}>
-        <LoadedMessagesList chatId={chatId} newMessages={newMessages} />
-        <NewMessagesList
-          key={chatId}
-          chatId={chatId}
-          newMessages={newMessages}
-          setNewMessages={setNewMessages}
-        />
+  function chatView() {
+    return (
+      <div className={styles.container}>
+        {isSmallScreen && (
+          <button onClick={() => setSelectedChatId(null)}>Back</button>
+        )}
+        <div className={styles.chat}>
+          <LoadedMessagesList chatId={chatId} newMessages={newMessages} />
+          <NewMessagesList
+            key={chatId}
+            chatId={chatId}
+            newMessages={newMessages}
+            setNewMessages={setNewMessages}
+          />
+        </div>
+        <SendMessage chatId={chatId} setNewMessages={setNewMessages} />
       </div>
-      <SendMessage chatId={chatId} setNewMessages={setNewMessages} />
-    </div>
-  );
+    );
+  }
+
+  return chatId ? chatView() : <p>Select chat to see messages!</p>;
 }
 
 export default ChatBox;
--- src/pages/inbox/Inbox.jsx
+++ src/pages/inbox/Inbox.jsx
@@ -9,40 +9,46 @@ function Inbox() {
   const [selectedChatId, setSelectedChatId] = useState(null);
   const isSmallScreen = window.matchMedia("(max-width: 768px)").matches;
 
-  return (
-    <div className={`${styles.inbox} d-flex`}>
-      {isSmallScreen ? (
-        selectedChatId ? (
-          <ChatBox
-            isSmallScreen={isSmallScreen}
-            chatId={selectedChatId}
+  function showOneView() {
+    return selectedChatId ? (
+      <ChatBox
+        isSmallScreen={isSmallScreen}
+        chatId={selectedChatId}
+        setSelectedChatId={setSelectedChatId}
+      />
+    ) : (
+      <div className={`${styles.sidebar} d-flex f-col bg-neutral-200`}>
+        <CreateGroupChat />
+        <ChatList
+          selectedChatId={selectedChatId}
+          setSelectedChatId={setSelectedChatId}
+        />
+      </div>
+    );
+  }
+
+  function showTwoViews() {
+    return (
+      <>
+        <div className={`${styles.sidebar} d-flex f-col bg-neutral-200`}>
+          <CreateGroupChat />
+          <ChatList
+            selectedChatId={selectedChatId}
             setSelectedChatId={setSelectedChatId}
           />
-        ) : (
-          <div className={`${styles.sidebar} d-flex f-col bg-neutral-200`}>
-            <CreateGroupChat />
-            <ChatList
-              selectedChatId={selectedChatId}
-              setSelectedChatId={setSelectedChatId}
-            />
-          </div>
-        )
-      ) : (
-        <>
-          <div className={`${styles.sidebar} d-flex f-col bg-neutral-200`}>
-            <CreateGroupChat />
-            <ChatList
-              selectedChatId={selectedChatId}
-              setSelectedChatId={setSelectedChatId}
-            />
-          </div>
-          {selectedChatId ? (
-            <ChatBox chatId={selectedChatId} />
-          ) : (
-            <h3>Please select user to start chat!</h3>
-          )}
-        </>
-      )}
+        </div>
+        <ChatBox
+          isSmallScreen={isSmallScreen}
+          chatId={selectedChatId}
+          setSelectedChatId={setSelectedChatId}
+        />
+      </>
+    );
+  }
+
+  return (
+    <div className={`${styles.inbox} d-flex`}>
+      {isSmallScreen ? showOneView() : showTwoViews()}
     </div>
   );
 }
