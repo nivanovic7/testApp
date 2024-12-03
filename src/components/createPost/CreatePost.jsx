14d4949c2777795ae50e6902e451488ae9c335da
Nikola Ivanovic <ivanovic.nikola.ns@gmail.com>
2024-12-03 10:37:04 +0100

    Added styles to create post page

--- src/components/createPost/CreatePost.jsx
+++ src/components/createPost/CreatePost.jsx
@@ -1,3 +1,5 @@
+import styles from "./CreatePost.module.css";
+
 import { useState } from "react";
 import { useCreateOutfitMutation } from "../../app/api/postApiSlice";
 import { useGetUserSettingsQuery } from "../../app/api/userApiSlice";
@@ -10,7 +12,6 @@ function CreatePost() {
     useCreateOutfitMutation();
   const { data } = useGetUserSettingsQuery();
 
-  console.log(data);
   const [description, setDescription] = useState("");
   const [image, setImage] = useState(null);
   const [video, setVideo] = useState(null);
@@ -38,40 +39,42 @@ function CreatePost() {
   if (!data) return <p>Loading...</p>;
 
   return (
-    <form onSubmit={handleSubmit}>
-      {error && <ErrorMessage error={error} />}
-      {isSuccess && <p>Post created!</p>}
+    <div className={`${styles.container} p-20`}>
+      <form onSubmit={handleSubmit}>
+        {error && <ErrorMessage error={error} />}
+        {isSuccess && <p>Post created!</p>}
 
-      <Input
-        onChange={(e) => setDescription(e.target.value)}
-        value={description}
-        label="Post description"
-        required
-      />
+        <Input
+          onChange={(e) => setDescription(e.target.value)}
+          value={description}
+          label="Post description"
+          required
+        />
 
-      <Input
-        label="Choose Images"
-        onChange={(e) => setImage(e.target.files)}
-        type="file"
-        multiple
-        name="file"
-        id="outfitsImage"
-        required
-      />
+        <Input
+          label="Choose Images"
+          onChange={(e) => setImage(e.target.files)}
+          type="file"
+          multiple
+          name="file"
+          id="outfitsImage"
+          required
+        />
 
-      <Input
-        label="Choose video"
-        onChange={(e) => setVideo(e.target.files[0])}
-        type="file"
-        name="file"
-        id="outfitsVideos"
-        required
-      />
+        <Input
+          label="Choose video"
+          onChange={(e) => setVideo(e.target.files[0])}
+          type="file"
+          name="file"
+          id="outfitsVideos"
+          required
+        />
 
-      <button disabled={isLoading} type="submit">
-        {isLoading ? "Loading..." : "Create"}
-      </button>
-    </form>
+        <button disabled={isLoading} type="submit">
+          {isLoading ? "Loading..." : "Create"}
+        </button>
+      </form>
+    </div>
   );
 }
 
